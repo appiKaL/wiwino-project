23e0e5dc8142128e875a7f73b7993414bd43ac95
mehme <mehmetbatar120@gmail.com>
2024-08-26 13:16:48 +0200

    query 7

---
+++ 07.py
@@ -0,0 +1,24 @@
+import sqlite3
+import pandas as pd
+
+db_path = './db/vivino.db'
+
+query = '''
+SELECT *
+FROM wines
+WHERE name LIKE '%Cabernet Sauvignon%'
+ORDER BY ratings_average DESC, ratings_count DESC
+LIMIT 5;
+'''
+
+conn = sqlite3.connect(db_path)
+
+df = pd.read_sql_query(query, conn)
+
+conn.close()
+
+csv_file_path = '07.csv'
+
+df.to_csv(csv_file_path, index=False)
+
+print(f"Data has been written to {csv_file_path}")
