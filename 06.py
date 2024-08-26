09908bbefc593d526f14a9cb88106755334834ed
mehme <mehmetbatar120@gmail.com>
2024-08-26 12:22:33 +0200

    query 7

--- 06.py
+++ 06.py
@@ -5,46 +5,19 @@ db_path = r"c:\Users\mehme\becode---\BECODE___PROJECTS\02.WIWINO_sql\wiwino\db\v
 
 conn = sqlite3.connect(db_path)
 
-query1 = '''
-SELECT 
-    r.country_code AS country_code, 
-    c.name AS country_name, 
-    avg(w.ratings_average) as average_rating
-FROM wines w
-JOIN regions r ON w.region_id = r.id
-join countries c ON r.country_code = c.code
-GROUP BY 
-    r.country_code, c.name
-ORDER BY
-    average_rating DESC;
-'''
-query2 = '''
-SELECT
-    r.country_code AS country_code,
-    c.name AS country_name,
-
-    AVG(v.ratings_average) AS average_rating
-FROM
-    vintages v
-JOIN wines w ON v.wine_id = w.id 
-JOIN regions r ON w.region_id = r.id
-join countries c ON r.country_code = c.code
-GROUP BY
-    r.country_code, c.name
-ORDER BY
-    average_rating DESC;
+query = '''
+SELECT *
+FROM wines
+WHERE name LIKE '%Cabernet Sauvignon%'
+ORDER BY ratings_average DESC, ratings_count DESC
+LIMIT 5;
 '''
 
-
-df1 = pd.read_sql_query(query1, conn)
-df2 = pd.read_sql_query(query2, conn)
+df = pd.read_sql_query(query, conn)
 
 conn.close()
+csv_file_path = '07.csv'
 
-combined_df = pd.concat([df1, df2], ignore_index=True)
+df.to_csv(csv_file_path, index=False)
 
-csv_file_path = '06.csv'
-
-df1.to_csv(csv_file_path, index=False)
-
-print(f"Data from the first query has been written to {csv_file_path}")
+print(f"Data has been written to {csv_file_path}")
